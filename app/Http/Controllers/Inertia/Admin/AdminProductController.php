f695dc444a63f6cf430d7f321f4e0a030a942b3e
Luybomir <volkovvolkov22122002@gmail.com>
2026-01-25 22:04:17 +0300

    добавлен makefile, изменен env.example

--- app/Http/Controllers/Inertia/Admin/AdminProductController.php
+++ app/Http/Controllers/Inertia/Admin/AdminProductController.php
@@ -4,25 +4,15 @@ namespace App\Http\Controllers\inertia\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ProductFilterRequest;
-use App\Models\Product;
-use App\Services\ProductService;
-use App\Services\CategoryService;
 use Inertia\Inertia;
 
 class AdminProductController extends Controller
 {
-    public function __construct(
-        private ProductService $productService,
-        private CategoryService $categoryService,
-    )
-    {}
-
     /**
      * Список товаров для администратора
      */
     public function index(ProductFilterRequest $request)
     {
-
         return Inertia::render('Admin/Products/Index');
     }
 
@@ -31,7 +21,6 @@ class AdminProductController extends Controller
      */
     public function create()
     {
-
         return Inertia::render('Admin/Products/Create');
     }
 
--- app/Http/Controllers/Inertia/Web/WebProductController.php
+++ app/Http/Controllers/Inertia/Web/WebProductController.php
@@ -3,19 +3,10 @@
 namespace App\Http\Controllers\Inertia\Web;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\ProductFilterRequest;
-use App\Models\Product;
-use App\Services\CategoryService;
-use App\Services\ProductService;
 use Inertia\Inertia;
 
 class WebProductController extends Controller
 {
-    public function __construct(
-        private ProductService $productService,
-        private CategoryService $categoryService,
-    )
-    {}
 
     /**
      * Главная страница: список товаров с пагинацией и фильтром
